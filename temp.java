896b1f1a31544e3e8420214e5e42af8fa55caaa9
GAYATRI11021 <167742484+GAYATRI11021@users.noreply.github.com>
2024-04-22 12:10:25 +0530

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String[] args) {
+        // Create an instance of MyClass
+        MyClass myObject = new MyClass();
+
+        // Call the displayMessage method
+        myObject.displayMessage();
+    }
+
+    // Method to display a message
+    public void displayMessage() {
+        System.out.println("Hello, world! This is a sample Java code.");
+    }
+}
